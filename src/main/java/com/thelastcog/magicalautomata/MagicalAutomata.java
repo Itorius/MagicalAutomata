6e1e1c3221eb6cd2d7389cee9517ff682376fdbb
Itorius <Itorius@users.noreply.github.com>
2018-07-29 21:22:15 +0200

    Renaming, added creative tab

---
+++ src/main/java/com/thelastcog/magicalautomata/MagicalAutomata.java
@@ -0,0 +1,40 @@
+package com.thelastcog.magicalautomata;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = MagicalAutomata.MODID, name = MagicalAutomata.NAME, version = MagicalAutomata.VERSION, acceptedMinecraftVersions = MagicalAutomata.ACCEPTED_MINECRAFT_VERSIONS)
+public class MagicalAutomata {
+    public static final String MODID = "magicalautomata";
+    public static final String NAME = "Magical Automata";
+    public static final String VERSION = "0.0.1";
+    public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12]";
+
+    @Mod.Instance
+    public static MagicalAutomata instance;
+
+    private static Logger logger;
+
+    static final CreativeTabs tabMagicalAutomata = (new CreativeTabs("tabMagicalAutomata") {
+        @Override
+        public ItemStack getTabIconItem() {
+            return new ItemStack(Items.BAKED_POTATO);
+        }
+    });
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+}
