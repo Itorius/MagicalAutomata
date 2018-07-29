b1d9dbbebef5349bb7866280c2e3f809e73b0bea
Itorius <Itorius@users.noreply.github.com>
2018-07-29 23:13:03 +0200

    Update build.gradle

--- src/main/java/com/thelastcog/magicalautomata/MagicalAutomata.java
+++ src/main/java/com/thelastcog/magicalautomata/MagicalAutomata.java
@@ -3,38 +3,45 @@ package com.thelastcog.magicalautomata;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = MagicalAutomata.MODID, name = MagicalAutomata.NAME, version = MagicalAutomata.VERSION, acceptedMinecraftVersions = MagicalAutomata.ACCEPTED_MINECRAFT_VERSIONS)
-public class MagicalAutomata {
+@Mod(modid = MagicalAutomata.MODID, name = MagicalAutomata.NAME, version = MagicalAutomata.VERSION, acceptedMinecraftVersions = MagicalAutomata.MC_VERSION)
+public class MagicalAutomata
+{
     public static final String MODID = "magicalautomata";
     public static final String NAME = "Magical Automata";
     public static final String VERSION = "0.0.1";
-    public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12]";
+    public static final String MC_VERSION = "[1.12]";
 
-    @Mod.Instance
+    @Mod.Instance(MagicalAutomata.MODID)
     public static MagicalAutomata instance;
 
     private static Logger logger;
 
-    static final CreativeTabs tabMagicalAutomata = (new CreativeTabs("tabMagicalAutomata") {
+    static final CreativeTabs tabMagicalAutomata = (new CreativeTabs("tabMagicalAutomata")
+    {
         @Override
-        public ItemStack getTabIconItem() {
+        public ItemStack getTabIconItem()
+        {
             return new ItemStack(Items.BAKED_POTATO);
         }
     });
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
+    public void preInit(FMLPreInitializationEvent event)
+    {
         logger = event.getModLog();
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event) {
+    public void init(FMLInitializationEvent event)
+    {
 
     }
 }
